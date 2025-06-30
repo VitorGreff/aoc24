e542a451fe308ff3222e6c485dc316fed0a6f7f4
greff <vitorgreffdias@gmail.com>
2025-06-29 21:56:12 -0300

    day10

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-import { day08a } from "./exercises/day08";
+import { day01a, day01b } from "./exercises/day01";
+import { day02a, day02b } from "./exercises/day02";
+import { day03a, day03b } from "./exercises/day03";
+import { day04a, day04b } from "./exercises/day04";
+import { day05a, day05b } from "./exercises/day05";
+import { day06a, day06b } from "./exercises/day06";
+import { day07a, day07b } from "./exercises/day07";
+import { day08a, day08b } from "./exercises/day08";
+import { day09a, day09b } from "./exercises/day09";
+import { day10a, day10b } from "./exercises/day10";
 
-console.log(day08a());
+console.log("Day 10a:", day10a());
+console.log("Day 10b:", day10b());
